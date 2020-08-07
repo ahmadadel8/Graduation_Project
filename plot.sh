fc4b1ba8582daa36ae9450a93be6bede656b3791
Ahmad Adel <ahmad@pop-os.localdomain>
2020-08-07 17:16:42 +0200

    plot.sh update

--- plot.sh
+++ plot.sh
@@ -9,14 +9,8 @@
 #SBATCH --output=plot.out
 
 
-python -u nutshell/plotter.py yolf_v1_coco.out fig_yolf_v1_coco.png
-python -u nutshell/plotter.py yolf_v1_50_coco.out fig_yolf_v1_50_coco.png
-python -u nutshell/plotter.py yolf_v2_coco.out fig_yolf_v2_coco.png
-python -u nutshell/plotter.py yolf_v2_35_coco.out fig_yolf_v2_35_coco.png
-python -u nutshell/plotter.py yolf_v1_voc.out fig_yolf_v1_voc.png
+
 python -u nutshell/plotter.py yolf_v1_50_voc.out fig_yolf_v1_50_voc.png
-python -u nutshell/plotter.py yolf_v2_voc.out fig_yolf_v2_voc.png
-python -u nutshell/plotter.py yolf_v2_35_voc.out fig_yolf_v2_35_voc.png
 python -u nutshell/plotter.py yolf_v1_50_lrshoot_voc.out fig_yolf_v1_50_lrshoot_voc.png
 
 
